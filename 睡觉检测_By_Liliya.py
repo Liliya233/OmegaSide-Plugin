291b22672641776cc186db63a2fda1d3ec359c07
Liliya233 <abc572024594@gmail.com>
2022-11-08 22:40:48 +0800

    修正文本

--- 睡觉检测_By_Liliya.py
+++ 睡觉检测_By_Liliya.py
@@ -16,20 +16,20 @@ class sleepDetect:
     # 判断是否满足跳过条件
     def isSatisfy(self, eventData):
         # Mojang ??
-        plTotal , plSlepping= int(eventData) // 65536, int(eventData) % 65536
+        plTotal , plsleeping= int(eventData) // 65536, int(eventData) % 65536
         # 人数变动时也会触发此事件，加个辨别吧
         # PS：如果不处于夜晚或者雷雨天
         if not (12000 <= int(self.api.do_send_ws_cmd("time query daytime").result.OutputMessages[0].Parameters[0]) <= 24000
             or self.api.do_send_ws_cmd("weather query").result.OutputMessages[0].Parameters[0] == "%commands.weather.query.thunder"):
             return
         # 判断睡觉的玩家是否满足设定的比例值
-        if plSlepping/plTotal >= self.sleepPercentage:
+        if plsleeping/plTotal >= self.sleepPercentage:
             self.api.send_all_player_msg("§b半数玩家已睡眠，将跳过黑夜或雷雨天")
             time.sleep(5)
             self.api.do_send_wo_cmd("time set 0")
             self.api.do_send_wo_cmd("weather clear")
         else:
-            self.api.send_all_player_msg(f"§b要想跳过黑夜或雷雨天，还需§e {int(plTotal/2)+1-plSlepping} §b名玩家进入睡眠")
+            self.api.send_all_player_msg(f"§b要想跳过黑夜或雷雨天，还需§e {int(plTotal/2)+1-plsleeping} §b名玩家进入睡眠")
         
     # 接收到特定事件数据包时进行处理
     def dealIDLevelEvent(self, packet):
